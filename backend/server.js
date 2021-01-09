a66c733919d85c9209f4e147b8497845cfa41f21
Abdelrahman Khaled <boodykhaled@gmail.com>
2021-01-09 17:15:14 +0200

    Prepare for deployment

--- backend/server.js
+++ backend/server.js
@@ -15,9 +15,9 @@ connectDB();
 const app = express();
 app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('API is running');
-});
+// app.get('/', (req, res) => {
+//   res.send('API is running');
+// });
 
 app.use('/api/products', productRoutes);
 app.use('/api/users', userRoutes);
@@ -30,6 +30,18 @@ app.use('/api/upload', uploadRoutes);
 
 const __dirname = path.resolve();
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '/frontend/build')));
+
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+  );
+} else {
+  app.get('/', (req, res) => {
+    res.send('API is running');
+  });
+}
 
 app.use(notFound);
 
